422a6e4924cdc4a890eb5cf740f126de6e221bc9
ganeshpatil386386 <ganeshpatil386386@gmail.com>
2024-09-29 21:45:03 +0530

    add SEO tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,41 @@ export default async function RootLayout({
   return (
     <html lang='en'>
       <head>
+        <title>devgancode</title>
         <link rel='icon' href={data.favicon || "/favicon.ico"} />
+        <title>devgancode</title>
+        <meta name='description' content='' />
+
+        <meta
+          property='og:url'
+          content='https://devgancode-blogs.vercel.app/'
+        />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='devgancode' />
+        <meta
+          property='og:description'
+          content='Explore insightful articles on web development, DevOps, backend technologies, and modern frontend styling. devgancode shares tutorials, tips, and best practices for aspiring developers and tech enthusiasts.'
+        />
+        <meta
+          property='og:image'
+          content='https://opengraph.b-cdn.net/production/images/bbc3e315-cdd2-473b-bc0e-0af8a1218e2c.png?token=hC99BhYl1WlU6Pmk1hryXictgpuVAAeWUd8aNpku9fQ&height=901&width=1200&expires=33263625503'
+        />
+
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta property='twitter:domain' content='devgancode-blogs.vercel.app' />
+        <meta
+          property='twitter:url'
+          content='https://devgancode-blogs.vercel.app/'
+        />
+        <meta name='twitter:title' content='devgancode' />
+        <meta
+          name='twitter:description'
+          content='Discover in-depth guides and articles on web development, backend, DevOps, and frontend styling. Join devgancode for practical tips and knowledge to boost your tech skills.'
+        />
+        <meta
+          name='twitter:image'
+          content='https://opengraph.b-cdn.net/production/images/bbc3e315-cdd2-473b-bc0e-0af8a1218e2c.png?token=hC99BhYl1WlU6Pmk1hryXictgpuVAAeWUd8aNpku9fQ&height=901&width=1200&expires=33263625503'
+        />
       </head>
       <body className={inter.className}>
         <Providers>
